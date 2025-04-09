92bdecac9a0e63db5fdc4c4dae47eda5adbb869e
intaky-dev <intaky@Intaky>
2025-04-09 19:55:30 -0300

    renombrar directorio

--- models/product_attribute.py
+++ models/product_attribute.py
@@ -1,12 +1,14 @@
 from odoo import models, fields
 
 class ProductAttribute(models.Model):
-    _inherit = ['product.attribute', 'mail.thread', 'mail.activity.mixin']
+    _inherit = 'product.attribute'  # Inherit the existing model
+    _mail_thread = True  # Enable mail.thread features
+    _mail_activity_mixin = True  # Enable mail.activity.mixin features
 
     name = fields.Char(
-        track_visibility='onchange'
+        tracking=True  # Add tracking to the 'name' field
     )
     display_type = fields.Selection(
         selection_add=[],
-        track_visibility='onchange'
+        tracking=True  # Add tracking to the 'display_type' field
     )
